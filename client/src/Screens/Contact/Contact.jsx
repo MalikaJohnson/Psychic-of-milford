14f8bd722e5218beff6a123f578599a33b9db1bc
Malika Johnson <malikajohnson1995@MacBook-Air.lan>
2021-09-10 18:16:15 -0400

    contact page

---
+++ client/src/Screens/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Contact() {
+  
+
+  return (
+    <div className='contact-container'>
+      <h2>Schedule a visit at the following location</h2>
+      <p>16 Daniel St. Milford, CT 06460</p>
+      <p><span className='contact-phone'>Phone</span>: (475)210-8562</p>
+      <p><span className='contact-hours'>Hours</span>
+        Monday - Thursday: 9AM - 9PM
+        <br />
+        Friday - Saturday: 9AM - 11PM
+        <br />
+        Sunday: 9AM - 6PM
+      </p>
+      <p className='contact-closing'>Your future is only a phone call away</p>
+    </div>
+  )
+}
